f07b064463f7d825c51f2f0a97cca6f06a62e677
Devan Goodwin <dgoodwin@rm-rf.ca>
2010-09-05 16:28:55 -0300

    Switch to Devise for login functionality.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Ftw::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
+  config.action_mailer.default_url_options = { :host => 'ftw.rm-rf.ca' }
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
