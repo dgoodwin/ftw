d67e0846e7895cffd9007fd679251612f4a6bb5a
Devan Goodwin <dgoodwin@rm-rf.ca>
2010-09-14 20:02:09 -0300

    Display league roles on members page.

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -1,2 +1,21 @@
 module LeaguesHelper
+
+  # Roles that are interesting to a league:
+  LEAGUE_ROLES = [
+    'league_admin',
+    'host',
+  ]
+
+  # Displays list of permissions relevant to this league:
+  def show_league_perms(league, user)
+    results = ""
+    user.permissions.each do |p|
+      if LEAGUE_ROLES.include?(p.role) and p.qualifier == league.id
+        results << ", " if results.length > 0
+        results << p.role 
+      end
+    end
+    return results
+  end
+
 end
